70cffffe61b6a5f07dd502dcc265ae7394903ae2
LuisAndresBuitrago <luisa.buitragom@uqvirtual.edu.co>
2026-05-26 00:42:12 -0500

    Inicio de los Test

---
+++ src/test/java/edu/pgii/eventos/service/ExportadorCsvReporteTest.java
@@ -0,0 +1,18 @@
+package edu.pgii.eventos.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExportadorCsvReporteTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void export() {
+    }
+}
---
+++ src/test/java/edu/pgii/eventos/service/ExportadorPdfSimpleTest.java
@@ -0,0 +1,22 @@
+package edu.pgii.eventos.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExportadorPdfSimpleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void export() {
+    }
+}
